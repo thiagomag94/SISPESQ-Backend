1ead17722af3273555666d7ad3cf80bc9bb1323b
thiagomag94 <thiagomag94@gmail.com>
2025-08-26 08:41:19 -0300

    periodicos qualis implementend excluding files

--- api/routes/Artigos.js
+++ api/routes/Artigos.js
@@ -1,26 +1,10 @@
-// Substitua o conteúdo de routes/Artigos.js por isto:
+
 
 const express = require('express');
 const router = express.Router();
 const artigosController = require('../controllers/artigosController');
 
-/**
- * @swagger
- * tags:
- * - name: Artigos
- * description: Endpoints para gerenciar artigos científicos.
- */
 
-/**
- * @swagger
- * /Artigos:
- * get:
- * summary: Lista todos os artigos da UFPE.
- * tags: [Artigos]
- * responses:
- * '200':
- * description: Uma lista de artigos foi retornada com sucesso.
- */
 router.get('/', artigosController.getTodosArtigosUFPE);
 
 // DEIXE AS OUTRAS ROTAS SEM COMENTÁRIOS POR ENQUANTO
